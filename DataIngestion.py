f160950d56b4a27ae4d245ea2415ef0fab1649a4
btierra <btierra@ucsd.edu>
2021-06-25 19:48:42 -0700

    change common_event function

--- DataIngestion.py
+++ DataIngestion.py
@@ -1,11 +1,10 @@
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DateType, TimestampType, DecimalType
-
 from pyspark.sql import SparkSession
 from typing import List
 import json
 
 def common_event(*args):
-    print(args)
+    return [arg for arg in args]
 
 def parse_csv(line:str):
     record_type_pos = 2
@@ -50,24 +49,23 @@ def parse_csv(line:str):
 
 spark = SparkSession.builder.master("local").appName("app").getOrCreate()
 spark.conf.set("fs.azure.account.key.equitymarketdatastorage.blob.core.windows.net","0Pjp/4C3REg7xPeNZulrdlcm85uSgj3mtonuvHyZcxNkDtvUyDmDqaum2rDj9qxucgJgHpLfDKCstiQ3UsMo8Q==")
-raw = spark.sparkContext.textFile("market-value/data/csv/2020-08-05/NYSE/part-00000-5e4ced0a-66e2-442a-b020-347d0df4df8f-c000.txt")
+raw = spark.sparkContext.textFile("data/csv/2020-08-05/NYSE/part-00000-5e4ced0a-66e2-442a-b020-347d0df4df8f-c000.txt")
 parsed = raw.map(lambda line: parse_csv(line))
-
-schema = StructType([ \
-    StructField("trade_dt", DateType(), True), \
-    StructField("rec_type", StringType(),True), \
-    StructField("symbol", StringType(),True), \
-    StructField("exchange", StringType(), True), \
-    StructField("event_tm", TimestampType(), True), \
-    StructField("event_seq_nb", IntegerType(), True), \
-    StructField("arrival_tm", TimestampType(), True), \
-    StructField("trade_pr", DecimalType(),True), \
-    StructField("bid_pr", DecimalType(),True), \
-    StructField("bid_size", IntegerType(), True), \
-    StructField("ask_pr", DecimalType(), True), \
-    StructField("ask_size", IntegerType(), True), \
-    StructField("partition", StringType(), True) \
-  ])
-
-#data = spark.createDataFrame(parsed)
+data = spark.createDataFrame(parsed)
 #data.write.partitionBy("partition").mode("overwrite").parquet("output_dir")
+
+# schema = StructType([ \
+#     StructField("trade_dt", DateType(), True), \
+#     StructField("rec_type", StringType(),True), \
+#     StructField("symbol", StringType(),True), \
+#     StructField("exchange", StringType(), True), \
+#     StructField("event_tm", TimestampType(), True), \
+#     StructField("event_seq_nb", IntegerType(), True), \
+#     StructField("arrival_tm", TimestampType(), True), \
+#     StructField("trade_pr", DecimalType(),True), \
+#     StructField("bid_pr", DecimalType(),True), \
+#     StructField("bid_size", IntegerType(), True), \
+#     StructField("ask_pr", DecimalType(), True), \
+#     StructField("ask_size", IntegerType(), True), \
+#     StructField("partition", StringType(), True) \
+#   ])
